222da4f1f00294856279c23ae12ebe0483df9dee
doxcom <aldoperez1319@gmail.com>
2024-11-28 21:33:00 -0600

    teacher table created and mapping to course

---
+++ src/main/java/com/dailycodebuffer/spring_data_jpa_tutorial/entity/Teacher.java
@@ -0,0 +1,41 @@
+package com.dailycodebuffer.spring_data_jpa_tutorial.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Teacher {
+
+  @Id
+  @SequenceGenerator(
+          name="teacher_sequence",
+          sequenceName = "teacher_sequence",
+          allocationSize=1
+  )
+  @GeneratedValue(
+          strategy = GenerationType.SEQUENCE,
+          generator = "teacher_sequence"
+  )
+  private Long teacherId;
+
+  private String firstName;
+  private String lastName;
+
+
+  @OneToMany(
+          cascade = CascadeType.ALL
+  )
+  @JoinColumn( //unidirectional one to many teacher can have a list of courses
+          name="teacher_id",
+          referencedColumnName = "teacherId" //an existing column on Teacher table
+  )
+  private List<Course> courses;
+
+}
